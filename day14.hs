3408d0682bbbb38339c80de660278a999bb08a3d
Jacob Xu <jxu023@ucr.edu>
2020-06-20 09:30:33 -0700

    commit before i rewrite it all -_-

--- day14.hs
+++ day14.hs
@@ -3,36 +3,54 @@ import ReactionTypes
 import ReactionParser
 import System.IO
 import qualified Data.Map.Strict as Map
-import qualified Data.Map.Merge.Strict as Merge
+import qualified Data.Set as Set
 
-timesAmount :: Int -> Term -> Term
-timesAmount x (chemical, amount) = (chemical, x * amount)
+type Set = Set.Set
 
-expand :: Reactions -> Term -> Pool
-expand reactions (chemical, needed) =
+data CFreq = CFreq { term :: Term
+                   , freq :: Int
+                   }
+instance Ord CFreq where
+    a `compare` b = freq a `compare` freq b
+
+type Pool = Set CFreq
+
+toCFreq :: Map String Int -> Term -> Int
+toCFreq freqMap term = CFreq Term (freqMap Map.! fst term)
+
+countFreqs :: Reactions -> Map String Int
+countFreqs reactions =
+    let incrementFreqs (_, terms) freqs   = foldr incrementTerm freqs terms
+        incrementTerm (chemical, _) freqs = Map.adjust (+ 1) chemical freqs
+        zeroFreqs                         = Map.fromList [(chemical, 0) | chemical <- map fst $ Map.toList reactions]
+    in foldr incrementFreqs zeroFreqs reactions
+
+timesTerm :: Int -> Term -> Term
+timesTerm x (chemical, amount) = (chemical, x * amount)
+
+expand :: Map String Int -> Reactions -> Term -> Pool
+expand freqs reactions (chemical, needed) =
     let (produced, inputs) = reactions Map.! chemical
         rounds             = ceiling (fromIntegral needed / fromIntegral produced)
-    in Map.fromList $ map (timesAmount $ trace (show rounds ++ " of " ++ chemical) rounds) inputs
-
-combinePools :: Pool -> Pool -> Pool
-combinePools = Merge.merge Merge.preserveMissing Merge.preserveMissing (Merge.zipWithMatched (\k u v -> u + v))
-
-sortChemicals :: Reactions -> [String]
-sortChemicals reactions =
-    let ins :: Map String Int
-        ins = Map.foldr countFormula zeroCount reactions
-        zeroCount = Map.fromSet (const 0) (Data.Set.fromList .
-    in
+        terms              = map (timesTerm rounds) inputs
+        pool               = Set.fromList $ map (toCFreq freqs) terms
+    in pool
 
 simplifyTerm :: Pool -> Reactions -> Pool
 simplifyTerm pool reactions =
-    let term@(chemical, _) = head . filter ((/= "ORE") . fst) . Map.toList $ pool
-        pool'              = Map.delete chemical pool
-        poolB              = expand reactions term
+    let term  = Set.elemAt 0 pool
+        pool' = Set.deleteAt 0 pool
+        poolB = expand freqs reactions term
     in combinePools pool' poolB
 
 minOre :: Reactions -> Int
-minOre reactions = go (expand reactions ("FUEL", 1))
+minOre reactions =
+    let freqs = countFreqs reactions
+        go :: Pool -> Int
+        go pool | Set.size pool == 1 = Set.elemAt 0 pool
+    in go (expand freqs reactions ("FUEL", 1))
+
+minOre reactions = go
     where go :: Pool -> Int
           go pool | Map.size pool == 1 = pool Map.! "ORE"
                   | otherwise          = go $ simplifyTerm pool reactions
