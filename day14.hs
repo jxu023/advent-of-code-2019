573b8a99e84c755a838e35b99277494b61351113
Jacob Xu <jxu023@ucr.edu>
2020-06-21 22:57:05 -0700

    advent day 14 part 2

--- day14.hs
+++ day14.hs
@@ -57,14 +57,14 @@ simplify process =
 
         basePool            = toPool terms (rank process)
         poolBuyMax          = scalePool (ceilingDivide needed produced) basePool
-    in trace (show $ pool process) $ process { pool = addPool poolCur poolBuyMax }
+    in process { pool = addPool poolCur poolBuyMax }
 
 onlyOre :: Pool -> Bool
 onlyOre pool = Map.size pool == 1 && (snd . fst $ Map.elemAt 0 pool) == "ORE"
 
-minOre :: Process -> Int
-minOre process | onlyOre (pool process) = snd $ Map.elemAt 0 (pool process)
-               | otherwise              = minOre $ simplify process
+consumeProcess :: Process -> Int
+consumeProcess process | onlyOre (pool process) = snd $ Map.elemAt 0 (pool process)
+                       | otherwise              = consumeProcess $ simplify process
 
 reverseMap :: Map String [String] -> Map String [String]
 reverseMap orig =
@@ -75,6 +75,7 @@ reverseMap orig =
             where add dst src m = Map.adjust (dst:) src m
     in foldr addEdges nodes kvs
 
+-- https://stackoverflow.com/questions/28549336/is-there-way-to-represent-static-data-in-haskell-or-is-there-any-other-elegant
 topoSort :: Map String [String] -> [String]
 topoSort m =
     let neighbors x = Map.findWithDefault [] x m
@@ -93,20 +94,35 @@ initRank reactions =
         len = length order
     in Map.fromList $ zip order [len-1,len-2..0]
 
-initProcess :: Reactions -> Process
-initProcess reactions =
+initProcess :: Int -> Reactions -> Process
+initProcess x reactions =
     let ranks = initRank reactions
-    in Process reactions ranks (toPool [("FUEL", 1)] ranks)
+    in Process reactions ranks (toPool [("FUEL", x)] ranks)
+
+-- some amount of fuel that needs more than 1 trillion ORE
+maxFuel :: Reactions -> Int
+maxFuel reactions = head . filter (> 1000000000000) . map (\x -> minOre (2^x) reactions) $ [1..]
+
+-- use binary search to find the max amount of FUEL which uses no more than 1 trillion ORE
+minOre :: Int -> Reactions -> Int
+minOre x reactions = consumeProcess . initProcess x $ reactions
+
+-- get highest value where pred is true
+binarySearch :: Int -> Int -> (Int -> Int) -> (Int -> Bool) -> Int
+binarySearch l r f pred | p && not p1 = m
+                        | pred mv     = binarySearch m r f pred
+                        | otherwise   = binarySearch l (m-1) f pred
+    where m = (l + r) `div` 2
+          mv = f m
+          p = pred mv
+          p1 = pred . f $ m + 1
 
 main :: IO()
 main = do
     reactions <- openFile "day14.input" ReadMode
                     >>= hGetContents
                     >>= return . parseReactions
-    print "reactions"
-    print reactions
-    print "rank"
-    print $ rank $ initProcess reactions
-    print "ore calculations"
-    let ores = minOre . initProcess $ reactions
-    putStrLn $ "need a minimum of " ++ show ores ++ " ORE"
+    putStrLn "maxFuel"
+    print $ maxFuel reactions
+    putStrLn "maxFuel for 1 trillion ore"
+    print $ binarySearch 0 (maxFuel reactions) (flip minOre reactions) (<= 1000000000000)
